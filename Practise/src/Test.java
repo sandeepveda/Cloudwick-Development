e27210af52d7db4cc50f356f8bb39697a474de05
Sandy <Sandy@192.168.1.9>
2015-11-04 12:57:36 -0500

    test commit

---
+++ Practise/src/Test.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class Test {
+
+	public static int testSum(int x, int y) {
+		int z = x + y;
+		return z;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi this is my new Mac");
+		int i = 5;
+		int j = 10;
+		// int z = i=j;
+		// System.out.println("Addition of i and j is="+z);
+		int w =Test.testSum(i, j);
+		System.out.println("The value of w is equal to"+  w);
+
+	}
+}
---
+++ Practise/src/com/hadoop/practise/MRTest.java
@@ -0,0 +1,10 @@
+package com.hadoop.practise;
+
+public class MRTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
